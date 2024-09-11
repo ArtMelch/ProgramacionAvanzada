ff2d2a0656a864d0bd2d13663d672149f998dd58
ArtMelch <elfalsoyoponce@gmail.com>
2024-09-11 09:52:43 -0600

    Tarea terminada

---
+++ circulo.py
@@ -0,0 +1,14 @@
+class Circulo:
+    radio = 0
+    
+    #Método constructor
+    def __init__(self, radio):
+        self.radio = radio
+    
+    def calcular_area(self):
+        self.area = round(pow(self.radio, 2) * 3.1416, 3)
+        print("Area: ", self.area)
+    
+    def calcular_perimetro(self):
+        self.perimetro = round(2 * 3.1416 * self.radio, 3)
+        print("Perimetro: ", self.perimetro)
---
+++ main.py
@@ -0,0 +1,20 @@
+from circulo import Circulo
+
+circulo_uno = Circulo(7)
+circulo_dos = Circulo(20)
+circulo_tres = Circulo(16)
+
+print("\n")
+print("Circulo uno")
+circulo_uno.calcular_area()
+circulo_uno.calcular_perimetro()
+
+print("\n")
+print("Circulo dos")
+circulo_dos.calcular_area()
+circulo_dos.calcular_perimetro()
+
+print("\n")
+print("Circulo tres")
+circulo_tres.calcular_area()
+circulo_tres.calcular_perimetro()
